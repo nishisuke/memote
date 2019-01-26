7c19373f16dada4b02ff93c1d23e59f147f4f1a2
nishisuke <nishisuke13+git@gmail.com>
2019-01-26 16:12:16 +0900

    impl auth

--- src/main.js
+++ src/main.js
@@ -1,7 +1,56 @@
 import firebase from 'firebase';
+import firebaseui from 'firebaseui';
 
 // Required for side-effects
-import 'firebase/firestore';
+// import 'firebase/firestore';
 
-console.log("hoge")
-console.log("hohogehoge")
+import ENV from 'ENV';
+
+firebase.initializeApp({
+  apiKey: ENV.SBA_NODE_FB_APIKEY,
+  authDomain: ENV.SBA_NODE_FB_AUTHDOMAIN,
+  projectId: ENV.SBA_NODE_FB_PROJECTID
+});
+
+//let db = firebase.firestore();
+//db.collection('texts').add({
+//  string: 'hoge'
+//})
+
+let initApp = () => {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      // User is signed in.
+      // let displayName = user.displayName;
+      // let email = user.email;
+      // let emailVerified = user.emailVerified;
+      // let photoURL = user.photoURL;
+      // let uid = user.uid;
+      // let phoneNumber = user.phoneNumber;
+      // let providerData = user.providerData;
+      user.getIdToken().then(accessToken => {
+        console.log(accessToken)
+      });
+    } else {
+      let uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        tosUrl: '<your-tos-url>', // TODO set url
+        privacyPolicyUrl: () => {
+          window.location.assign('<your-privacy-policy-url>'); // TODO set url
+        }
+      };
+
+      let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+      // The start method will wait until the DOM is loaded.
+      if (ui.isPendingRedirect()) {
+        ui.start('#firebaseui-auth-container', uiConfig);
+      }
+    }
+  }, console.log);
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
