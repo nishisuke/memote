fa6491bfcc812e32cd633ed7c5ca8fd2f1f7b505
nishisuke <nishisuke13+git@gmail.com>
2019-01-29 21:29:27 +0900

    install react

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = env => ({
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
