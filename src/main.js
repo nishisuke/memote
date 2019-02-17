c8e7f65c2b6c68452bc5028a9c5fd8dedf3ca175
nishisuke <nishisuke13+git@gmail.com>
2019-02-17 14:48:46 +0900

    consoledev

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import initApp from './init'
 document.addEventListener('DOMContentLoaded', initApp);
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
