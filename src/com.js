cfc100639741ee0147045a95d040ca0904b25ded
nishisuke <nishisuke13+git@gmail.com>
2019-02-21 13:30:02 +0900

    height

--- src/com.js
+++ src/com.js
@@ -89,6 +89,15 @@ export default class MainPage extends React.Component {
   render() {
     return (
       <div className='scroll'>
+        <p>{window.innerHeight}</p>
+        <p>{window.outerHeight}</p>
+        <p>{window.parent.screen.height}</p>
+        <p>{window.parent.screen.availHeight}</p>
+        <p>{screen.height}</p>
+        <p>{screen.availHeight}</p>
+        <p>{document.body.clientHeight}</p>
+        <p>{document.documentElement.clientHeight}</p>
+      
         <button className='button is-medium' onClick={this.out}>out</button>
         <div className={`modal${this.state.showModal ? ' is-active' : ''}`}>
           <div className="modal-background"></div>
