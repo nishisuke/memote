9f9e57fb767a52790c35cb56b6b60895184994df
nishisuke <nishisuke13+git@gmail.com>
2019-02-25 12:59:38 +0900

    dix

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'SBA-cache-v2';
+var cacheName = 'SBA-cache-v3';
 var staticPathsToCache = [
   '/',
 ];
@@ -6,11 +6,38 @@ var staticPathsToCache = [
 var dynamicPathsToCache = serviceWorkerOption.assets
 
 self.addEventListener('install', function(event) {
-  console.log('a')
   event.waitUntil(
     caches.open(cacheName)
       .then(function(cache) {
         return cache.addAll(staticPathsToCache.concat(dynamicPathsToCache));
       })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(name) {
+          if (name != cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (navigator.onLine || !response) {
+          return fetch(event.request);
+        } else {
+          return response;
+        }
+      }
+    )
   );
 });
