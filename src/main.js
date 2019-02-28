5434361abbc7ce76e0062dd4faedfd0a4a31ebed
nishisuke <nishisuke13+git@gmail.com>
2019-02-28 09:41:45 +0900

    fi

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import './main.css'
 import firebase from 'firebase/app';
-import firebaseui from 'firebaseui';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import ENV from 'ENV';
-import SignedInContainer from './sp/signed_in_container'
-import PC from './pc/pc'
 
 firebase.initializeApp({
   apiKey: ENV.SBA_NODE_FB_APIKEY,
@@ -14,14 +7,26 @@ firebase.initializeApp({
   projectId: ENV.SBA_NODE_FB_PROJECTID
 });
 
+
 import db from './db'
 db.setup()
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SignedInContainer from './sp/signed_in_container'
+import PC from './pc/pc'
 
 if (window.innerWidth < 560) {
   ReactDOM.render(<SignedInContainer />, document.getElementById('main'))
 } else {
   ReactDOM.render(<PC />, document.getElementById('main'))
 }
+
+
+import './main.css'
+
 
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
