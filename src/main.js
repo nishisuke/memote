cdca62bd56053c5621a1dc6c5a7c6e0c66af76e0
nishisuke <nishisuke13+git@gmail.com>
2019-02-28 10:14:35 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -8,6 +8,40 @@ firebase.initializeApp({
 });
 
 
+import firebaseui from 'firebaseui';
+
+firebase.auth().onAuthStateChanged(user => {
+  if (!user) {
+    let uiConfig = {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        {
+          provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          scopes: [
+            'https://www.googleapis.com/auth/calendar.events',
+          ],
+          customParameters: {
+            // Forces account selection even when one account
+            // is available.
+            prompt: 'select_account'
+          }
+        },
+      ],
+      tosUrl: '<your-tos-url>', // TODO set url
+      privacyPolicyUrl: () => {
+        window.location.assign('<your-privacy-policy-url>'); // TODO set url
+      }
+    };
+
+    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+}, console.error);
+
+
 import db from './db'
 db.setup()
 
--- src/sp/signed_in_container.js
+++ src/sp/signed_in_container.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import firebase from 'firebase/app';
-import firebaseui from 'firebaseui';
 
 import Archive from './archived'
 import Main from './mainPage'
@@ -19,52 +17,6 @@ export default class SignedInContainer extends React.Component {
     this.goMain = this.goMain.bind(this);
     this.isArchive = this.isArchive.bind(this);
     this.goArchive = this.goArchive.bind(this);
-  }
-
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        // User is signed in.
-        // let displayName = user.displayName;
-        // let email = user.email;
-        // let emailVerified = user.emailVerified;
-        // let photoURL = user.photoURL;
-        // let uid = user.uid;
-        // let phoneNumber = user.phoneNumber;
-        // let providerData = user.providerData;
-        // user.getIdToken().then(accessToken => {
-        // console.log(accessToken)
-        // });
-      } else {
-
-        let uiConfig = {
-          signInSuccessUrl: '/',
-          signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            {
-              provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-              scopes: [
-                'https://www.googleapis.com/auth/calendar.events',
-              ],
-              customParameters: {
-                // Forces account selection even when one account
-                // is available.
-                prompt: 'select_account'
-              }
-            },
-          ],
-          tosUrl: '<your-tos-url>', // TODO set url
-          privacyPolicyUrl: () => {
-            window.location.assign('<your-privacy-policy-url>'); // TODO set url
-          }
-        };
-
-        let ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-        // The start method will wait until the DOM is loaded.
-        ui.start('#firebaseui-auth-container', uiConfig);
-      }
-    }, console.error);
   }
 
   isMain() { return this.state.innerPath === '/main' }
