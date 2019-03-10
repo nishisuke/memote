7d8d6f07aec01084279caeee34071ed8514a9a37
nishisuke <nishisuke13+git@gmail.com>
2019-03-10 15:02:44 +0900

    fix

--- src/component/modal.js
+++ src/component/modal.js
@@ -1,82 +1,79 @@
 import React from 'react'
 import db from '../db'
 
-export default class Modal extends React.Component {
+export default class Modal extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    let d = {...props.docData};
+    let id = d.id
+    delete d.id
     this.state = {
-      value: props.docData.string || '',
+      value: d.string || '',
+      docData: d,
+      docID: id,
       storeState: 'de',
-      timeoutID: -1,
+      timeoutID: null,
+      failText: null,
     };
 
-    this.queueCurrentCondition = this.queueCurrentCondition.bind(this);
+    this.cancelSaving = this.cancelSaving.bind(this);
+    this.saveTextAfter = this.saveTextAfter.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.hideModal = this.hideModal.bind(this);
     this.colorClass = this.colorClass.bind(this);
   }
 
   colorClass() {
-    let s = this.state.storeState
-    if ('stored' === s) return 'is-success';
-    if ('shouldSaveImi' === s) return 'is-danger';
-    if ('setTimeout' === s) return 'is-warning';
-
-    return ''
+    switch(this.state.storeState) {
+      case 'saved':        return 'is-success';
+      case 'savingFailed': return 'is-danger';
+      case 'de':           return '';
+      default:             return 'is-warning';
+    }
   }
 
   handleChange(event) {
     this.setState({
       value: event.target.value,
-      storeState: 'shouldSave',
+      storeState: 'shouldBeSaved',
     });
   }
 
   hideModal() {
-    clearTimeout(this.state.timeoutID)
-
-    if (this.state.storeState === 'stored' || this.state.storeState === 'de') {
-      this.props.unmountMe()
-    } else {
-    let copy = {...this.props.docData}
-    let id = copy.id
-    delete copy.id
-    let text = this.state.value
-    Object.assign(copy, { string: text })
-
-    db.putMemo(id, copy)
-      .then(this.props.unmountMe)
-      .catch(() => {
-        alert(`save failed: ${text}`)
-this.props.unmountMe()
-      });
-    }
+    this.setState({storeState: 'willBeFinished'})
   }
 
-  queueCurrentCondition() {
+  cancelSaving() {
     clearTimeout(this.state.timeoutID)
+  }
 
-    let copy = {...this.props.docData}
-    let id = copy.id
-    delete copy.id
+  saveTextAfter(milsec) {
     let text = this.state.value
-    Object.assign(copy, { string: text })
+    let d = Object.assign({}, this.state.docData, { string: text })
 
     let timeoutID = setTimeout(() => {
-    db.putMemo(id, copy)
-      .then(() => {
-        if (this.state.value === text) {
-          this.setState({storeState: 'stored'})
-        }
-      })
-      .catch(() => {
-        this.setState({storeState: 'shouldSaveImi', failText: text})
-      });
-    }, this.state.storeState === 'shouldSaveImi' ? 100 : 1500);
+      this.setState({storeState: 'beingSaved', timeoutID: null, failText: null})
+      db.putMemo(this.state.docID, d)
+        .then(() => {
+          if (this.state.storeState === 'beingSaved') {
+            this.setState({storeState: 'saved'})
+          } else if (this.state.storeState === 'willBeFinished') {
+            this.setState({storeState: 'canBeFinished'})
+          }
+        })
+        .catch(() => {
+          if (this.state.storeState === 'willBeFinished') {
+            alert(`save fail: ${text}`)
+            this.setState({storeState: 'canBeFinished'})
+          } else {
+            this.setState({storeState: 'savingFailed', failText: text})
+          }
+        });
+    }, milsec);
     this.setState({
       timeoutID: timeoutID,
-      storeState: 'setTimeout',
+      storeState: 'willBeSaved',
     })
   }
 
@@ -84,13 +81,46 @@ this.props.unmountMe()
     document.getElementById('ta').focus()
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.docData.id !== this.props.docData.id) {
+      alert('予期しないケースが発生しました。アプリを終了してください');
+      return;
+    }
+
+    console.log(this.state.storeState)
     switch(this.state.storeState) {
-      case  'shouldSave':
-        this.queueCurrentCondition()
+      case 'shouldBeSaved':
+        this.cancelSaving();
+        this.saveTextAfter(1500)
         break;
-      case 'shouldSaveImi':
-        this.queueCurrentCondition()
+      case 'savingFailed':
+        if (prevState.storeState === 'beingSaved') {
+          this.cancelSaving();
+          this.saveTextAfter(100)
+        }
+        break;
+      case 'willBeFinished':
+        switch(prevState.storeState) {
+          case 'willBeSaved':
+            this.cancelSaving()
+            let text = this.state.value
+            let d = Object.assign({}, this.state.docData, { string: text })
+
+            db.putMemo(this.state.docID, d)
+              .catch(() => {
+                alert(`save failed: ${text}`)
+              });
+            this.setState({ storeState: 'canBeFinished' })
+            break;
+          case 'beingSaved':
+            break;
+          default:
+            this.setState({ storeState: 'canBeFinished' })
+            break;
+        }
+        break;
+      case 'canBeFinished':
+        this.props.unmountMe()
         break;
     }
   }
@@ -98,7 +128,7 @@ this.props.unmountMe()
   render() {
     return (
       <div className='modal-content'>
-        <div className={`control ${this.state.storeState === 'setTimeout' ? 'is-loading' : ''}`}>
+        <div className={`control ${this.state.storeState === 'willBeSaved' ? 'is-loading' : ''}`}>
           <textarea id='ta' onChange={this.handleChange} value={this.state.value} onBlur={this.hideModal} className={`textarea has-fixed-size ${this.colorClass()}`} rows='12' />
         </div>
       </div>
