d598044a82b18eed32dd0ab984e6d6613f88819e
nishisuke <nishisuke13+git@gmail.com>
2019-03-14 18:17:21 +0900

    fi

--- src/container/texts.js
+++ src/container/texts.js
@@ -1,11 +1,12 @@
 import React, { useReducer, useEffect, useMemo } from 'react'
 
 import db from '../db'
+import useSubscribeTexts from '../hooks/useSubscribeTexts'
+
 import OpenedModal from '../component/modal'
 import Text from '../component/text'
 import Menu from '../component/menu'
 import TextEditor from '../component/textarea'
-import useSubscribeTexts from '../hooks/useSubscribeTexts'
 
 const ACTION_MENU = 'menu'
 const ACTION_HIDE_MENU = 'hideMenu'
@@ -42,16 +43,17 @@ export default props => {
   const showMenu = () => dispatch({ type: ACTION_MENU })
   const hideMenu = () => dispatch({ type: ACTION_HIDE_MENU })
   const setNewEditing = () => setEditingFunc(db.newMemo().id)()
-  const finishEditing = () => dispatch({ type: ACTION_FINISH_EDITING })
+  const finishEditing = useMemo(() => (() => dispatch({ type: ACTION_FINISH_EDITING })), [])
   const setEditingFunc = id => (() => dispatch({ type: ACTION_EDITING, id: id }))
 
-  const editing = useMemo(() => (texts.find(t => t.id === state.editingID) || { id: state.editingID, string: '' }), [state.editingID])
+  const editing = texts.find(t => t.id === state.editingID) || { id: state.editingID, string: '' }
+  const memoEditing = useMemo(() => (texts.find(t => t.id === state.editingID) || { id: state.editingID, string: '' }), [editing.id, editing.string])
 
   return (
     <div className='rootContainer'>
       <div className={`modal ${state.showTextareaModal && window.innerWidth < 560 ? 'is-active' : ''}`}>
         <div className='modal-background' onClick={finishEditing}></div>
-        <OpenedModal unmountMe={finishEditing} docData={editing}/>
+        <OpenedModal unmountMe={finishEditing} docData={memoEditing}/>
       </div>
 
       <div className={`modal ${state.showMenu ? 'is-active' : ''}`}>
@@ -62,7 +64,7 @@ export default props => {
       <div className='CMain'>
         <div className='CTexts'>{ texts.map(text => <Text setEdit={setEditingFunc(text.id)} key={text.id} data={text} edit={setEditingFunc(text.id)} />)}</div>
         <div className='inputContainer'>
-          <TextEditor docData={editing} />
+          <TextEditor docData={memoEditing} />
 
           <div className='fixedActionContainer'>
             <div id='archiveIcon' className='has-text-danger is-invisible'><span className='icon is-large'><i className='fas fa-archive fa-2x'></i></span></div>
