af7d891013b865d6e5af2945483d6d33a35bd012
nishisuke <nishisuke13+git@gmail.com>
2019-03-15 10:02:13 +0900

    rouhook

--- src/container/routing.js
+++ src/container/routing.js
@@ -1,39 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import firebase from 'firebase/app'
 
 import Archive from './archived'
 import Texts from './texts'
 
-export default class Routing extends React.Component {
-  constructor(props) {
-    super(props)
+export default () => {
+  const [signed, setSigned] = useState(false);
 
-    this.state = {
-      signed: false,
-    }
-  }
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => setSigned(!!user))
+  }, [])
 
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({ signed: true })
-      } else {
-        this.setState({ signed: false })
-      }
-    })
-  }
+  if (!signed) return <p>signing in</p>;
 
-  render() {
-    if (!this.state.signed) return <p>signing in</p>;
-
-    return (
-      <Router>
-        <div>
-          <Route path='/' exact component={Texts} />
-          <Route path='/archives/' component={Archive} />
-        </div>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <div>
+        <Route path='/' exact component={Texts} />
+        <Route path='/archives/' component={Archive} />
+      </div>
+    </Router>
+  )
 }
