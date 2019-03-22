7b5061bc9e43693d0363ca110b4e0c1160a484d5
nishisuke <nishisuke13+git@gmail.com>
2019-03-22 11:53:11 +0900

    WIPPC

--- src/container/texts.js
+++ src/container/texts.js
@@ -10,6 +10,42 @@ import Modal from '../component/Modal'
 import Menu from '../component/menu'
 
 export default () => {
+  return window.innerWidth < 560 ? <SP /> : <PC />
+}
+
+const PC = () => {
+  const autoSave = useAutoSave()
+
+  // editor
+  const editorRef = useRef(null)
+  useLayoutEffect(() => {
+    if (autoSave.isEditing) editorRef.current.focus();
+  }, [autoSave.isEditing])
+
+  // menu
+  const [showMenu, setShowMenu] = useState(false)
+  const menu = useMemo(() => <Menu />, [])
+
+  return (
+    <div className='rootContainer'>
+      <Modal isActive={showMenu} inactivate={() => setShowMenu(false)} content={menu} />
+
+      <div className='CMain'>
+        <TextsComponent startEditing={autoSave.startEditing} />
+        <div className='inputContainer'>
+          <div className='editorContainer'><textarea ref={editorRef} onChange={autoSave.change} value={autoSave.value} /></div>
+          <div className='fixedActionContainer'>
+            <div id='archiveIcon' className='has-text-danger is-invisible'><span className='icon is-large'><i className='fas fa-archive fa-2x'></i></span></div>
+            <div id='add' className='has-text-primary' onClick={() => autoSave.startEditing(db.newMemo())}><span className='icon is-large'><i className='fas fa-pen fa-2x'></i></span></div>
+            <div id='menu' onClick={() => setShowMenu(true)}><span className='icon is-large'><i className='fas fa-bars fa-2x'></i></span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const SP = () => {
   const autoSave = useAutoSave()
 
   // editor
