db269872a8cdef232dce732c6aca34d60db4c5d2
nishisuke <nishisuke13+git@gmail.com>
2019-03-24 12:33:01 +0900

    fixspdiv

--- src/container/texts.js
+++ src/container/texts.js
@@ -59,20 +59,16 @@ const SP = () => {
   const menu = useMemo(() => <Menu />, [])
 
   return (
-    <div className='rootContainer'>
+    <React.Fragment>
       <Modal isActive={autoSave.isEditing} inactivate={autoSave.finishEditing} content={<Editor finish={autoSave.finishEditing} ref={editorRef} handleChange={autoSave.change} value={autoSave.value} />} />
       <Modal isActive={showMenu} inactivate={() => setShowMenu(false)} content={menu} />
 
-      <div className='CMain'>
-        <TextsComponent startEditing={autoSave.startEditing} />
-        <div className='inputContainer'>
-          <div className='fixedActionContainer'>
-            <div id='archiveIcon' className='has-text-danger is-invisible'><span className='icon is-large'><i className='fas fa-archive fa-2x'></i></span></div>
-            <div id='add' className='has-text-primary' onClick={() => autoSave.startEditing(db.newMemo())}><span className='icon is-large'><i className='fas fa-pen fa-2x'></i></span></div>
-            <div id='menu' onClick={() => setShowMenu(true)}><span className='icon is-large'><i className='fas fa-bars fa-2x'></i></span></div>
-          </div>
-        </div>
+      <TextsComponent startEditing={autoSave.startEditing} />
+      <div className='fixedActionContainer'>
+        <div id='archiveIcon' className='has-text-danger is-invisible'><span className='icon is-large'><i className='fas fa-archive fa-2x'></i></span></div>
+        <div id='add' className='has-text-primary' onClick={() => autoSave.startEditing(db.newMemo())}><span className='icon is-large'><i className='fas fa-pen fa-2x'></i></span></div>
+        <div id='menu' onClick={() => setShowMenu(true)}><span className='icon is-large'><i className='fas fa-bars fa-2x'></i></span></div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
