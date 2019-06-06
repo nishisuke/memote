a6a0f391cc57c41d982717784f59a8556d54a20f
nishisuke <ryoskns+git@gmail.com>
2019-06-07 08:37:20 +0900

    page

--- src/component/texts/sp.js
+++ src/component/texts/sp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactGA from 'react-ga'
+import SwipeableViews from 'react-swipeable-views';
 
 import db from '../../db'
 import useAutoSave from '../../hooks/useAutoSave'
@@ -9,6 +10,16 @@ import Editor from '../editor'
 import TextsComponent from '../TextsComponent'
 import Modal from '../Modal'
 import Menu from '../menu'
+
+
+const styles = {
+   mainPage: {
+     height: '100vh',
+   },
+   oldMemoListPage: {
+     height: '100vh',
+   },
+}
 
 export default () => {
   const autoSave = useAutoSave()
@@ -37,12 +48,30 @@ export default () => {
       <Modal isActive={autoSave.isEditing} inactivate={autoSave.finishEditing} content={<Editor finish={autoSave.finishEditing} ref={editorRef} handleChange={autoSave.change} value={autoSave.value} />} />
       <Modal isActive={showMenu} inactivate={() => setShowMenu(false)} content={menu} />
 
-      <TextsComponent startEditing={autoSave.startEditing} />
-      <div className='fixedActionContainer'>
-        <div id='archiveIcon' className='has-text-danger is-invisible'><span className='icon is-large'><i className='fas fa-archive fa-2x'></i></span></div>
-        <div id='add' className='has-text-primary' onClick={onCreate}><span className='icon is-large'><i className='fas fa-pen fa-2x'></i></span></div>
-        <div id='menu' onClick={() => setShowMenu(true)}><span className='icon is-large'><i className='fas fa-bars fa-2x'></i></span></div>
-      </div>
+      <SwipeableViews>
+        <div style={{ ...styles.mainPage }}>
+          <TextsComponent startEditing={autoSave.startEditing} />
+          <div className='fixedActionContainer'>
+            <div id='archiveIcon' className='has-text-danger is-invisible'>
+              <span className='icon is-large'>
+                <i className='fas fa-archive fa-2x'></i>
+              </span>
+            </div>
+            <div id='add' className='has-text-primary' onClick={onCreate}>
+              <span className='icon is-large'>
+                <i className='fas fa-pen fa-2x'></i>
+              </span>
+            </div>
+            <div id='menu' onClick={() => setShowMenu(true)}>
+              <span className='icon is-large'>
+                <i className='fas fa-bars fa-2x'></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div style={{...styles.oldMemoListPage}}>
+        </div>
+      </SwipeableViews>
     </React.Fragment>
   )
 }
