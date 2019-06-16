4ac297658554020b8911a426cc554fc85eb80a15
nishisuke <ryoskns+git@gmail.com>
2019-06-16 13:12:59 +0900

    fc

--- src/components/text.js
+++ src/components/text.js
@@ -12,76 +12,77 @@ const restrictedPoint = (numX, numY) => {
 const shouldArchive = (touch, state) => {
   let left = touch.pageX - (state.offsetX || 0)
   let bottom = touch.pageY - (state.offsetY || 0) + 28
-  return (left < 96 + ((window.innerWidth - 248) / 6) && window.innerHeight - bottom < 72)
+  return (left < 96 + ((window.innerWidth - 248) / 6) && window.innerHeight - bottom < 104)
 }
 
-export default class TextComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    const d = props.data
-    const x = (d ? d.pageXRate * window.innerWidth : 0)
-    const y = (d ? d.pageYRate * window.innerHeight : 0)
+export default (props) => {
+  const d = props.data
+  const x = (d ? d.pageXRate * window.innerWidth : 0)
+  const y = (d ? d.pageYRate * window.innerHeight : 0)
+  const [point, setPoint] = React.useState({
+    pageX: x,
+    pageY: y,
+  })
+  const [offset, setOffset] = React.useState({
+    offsetX: 0,
+    offsetY: 0,
+  })
+  const [willArchive, setWillArchive] = React.useState(false)
 
-    this.state = {
-      pageX: x,
-      pageY: y,
-    };
+  const autoDelClassName = props.data.autoDeleteAt ? 'auto-del' : ''
+  const willArchiveClassName = willArchive ? 'willArchive' : ''
 
+  const onTouchStart = event => {
+    props.setSlidable(false)
+    if (event.targetTouches.length == 1) {
+      let touch = event.targetTouches[0]
+      setOffset({ offsetX: touch.pageX - point.pageX, offsetY: touch.pageY - point.pageY })
+    }
+    return false
+  }
+  const onTouchMove = event => {
+    document.getElementById('archiveIcon').classList.remove('is-invisible')
+    document.getElementById('add').classList.add('is-invisible')
+    document.getElementById('menu').classList.add('is-invisible')
+    if (event.targetTouches.length == 1) {
+      let touch = event.targetTouches[0]
+      let pageX = touch.pageX - (offset.offsetX || 0)
+      let pageY = touch.pageY - (offset.offsetY || 0)
+      setPoint(restrictedPoint(pageX, pageY))
+      if (shouldArchive(touch, offset)) {
+        setWillArchive(true)
+      } else {
+        setWillArchive(false)
+      }
+    }
+  }
+  const onTouchEnd = event => {
+    props.setSlidable(true)
+    document.getElementById('archiveIcon').classList.add('is-invisible')
+    document.getElementById('add').classList.remove('is-invisible')
+    document.getElementById('menu').classList.remove('is-invisible')
+    if (event.targetTouches.length == 0) {
+      let touch = event.changedTouches[0]
+      if (shouldArchive(touch, offset)) {
+        db.archiveMemo(props.data.id)
+      } else {
+        db.updatePoint(props.data.id, point.pageX / window.innerWidth, point.pageY / window.innerHeight)
+      }
+    }
+    return false
   }
 
-  componentDidMount() {
-    let ele = document.getElementById(this.props.data.id)
-    ele.addEventListener('touchstart', event => {
-      this.props.setSlidable(false)
-      if (event.targetTouches.length == 1) {
-        let touch = event.targetTouches[0]
-        this.setState({ offsetX: touch.pageX - this.state.pageX, offsetY: touch.pageY - this.state.pageY })
-      }
-      return false
-    }, { passive: false })
-
-    ele.addEventListener('touchmove', event => {
-      event.preventDefault()
-
-      document.getElementById('archiveIcon').classList.remove('is-invisible')
-      document.getElementById('add').classList.add('is-invisible')
-      document.getElementById('menu').classList.add('is-invisible')
-      if (event.targetTouches.length == 1) {
-        let touch = event.targetTouches[0]
-        let pageX = touch.pageX - (this.state.offsetX || 0)
-        let pageY = touch.pageY - (this.state.offsetY || 0)
-        this.setState(restrictedPoint(pageX, pageY))
-        if (shouldArchive(touch, this.state)) {
-          ele.classList.add('willArchive')
-        } else {
-          ele.classList.remove('willArchive')
-        }
-      }
-    }, { passive: false })
-
-    ele.addEventListener('touchend', event => {
-      this.props.setSlidable(true)
-      document.getElementById('archiveIcon').classList.add('is-invisible')
-      document.getElementById('add').classList.remove('is-invisible')
-      document.getElementById('menu').classList.remove('is-invisible')
-      if (event.targetTouches.length == 0) {
-        let touch = event.changedTouches[0]
-        if (shouldArchive(touch, this.state)) {
-          db.archiveMemo(this.props.data.id)
-        } else {
-          db.updatePoint(this.props.data.id, this.state.pageX / window.innerWidth, this.state.pageY / window.innerHeight)
-        }
-      }
-      return false
-    }, { passive: false })
-  }
-
-  render() {
-    const autoDelClassName = this.props.data.autoDeleteAt ? 'auto-del' : ''
-    return (
-      <div style={{ left: this.state.pageX + 'px', top: this.state.pageY + 'px' }} className={`${autoDelClassName} moveabs textlabel is-size-7` } onClick={() => this.props.edit(this.props.data)} id={this.props.data.id}>
-      { this.props.data.headText() }
-      </div>
-    )
-  }
+  return (
+    <div
+      style={{ left: point.pageX + 'px', top: point.pageY + 'px' }}
+      className={`${autoDelClassName} ${willArchiveClassName} moveabs textlabel is-size-7` }
+      onClick={() => props.edit(props.data)}
+      id={props.data.id}
+      onTouchEnd={onTouchEnd}
+      onTouchMove={onTouchMove}
+      onTouchStart={onTouchStart}
+    >
+      { props.data.headText() }
+    </div>
+  )
 }
